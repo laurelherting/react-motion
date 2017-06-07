00679dbb5414ecce0f675d383e6481ac2a544c21
Laurel Herting <laurel.herting@gmail.com>
2017-06-07 09:57:14 -0600

    wip motion

--- src/index.js
+++ src/index.js
@@ -1,88 +1,15 @@
 import React from 'react';
+import {Motion, spring } from 'react-motion';
 import ReactDOM from 'react-dom';
-import { Spring, presets } from 'react-motion';
-
-/* class App extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-      <div>animation goes here</div>
-    )
-  }
-}
-*/
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      items: Array.from(Array(18).keys())
-    };
-  }
   render() {
     return (
-      <div>animation goes here</div>
+    <Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
+      {value => {<div>{value.x}</div>}
+      }}
+      </motion>
     )
   }
 }
-
-  getDefaults() {
-    let obj = {};
-    this.state.items.forEach(key =>{
-      obj[key] = {
-        val: {
-          rotate: key % 2 === 0 ? 360 : -360,
-          scale: key % 2 === 0 ? 2 : 0
-        }
-      }
-    })
-    return obj; 
-  }
-
-  getEnds() {
-    let obj = {};
-    this.state.items.forEach(key =>{
-      obj[key] = {
-        val: {
-          rotate: 0,
-          scale: 1
-        }
-      }
-    })
-    return obj;
-  }
-
-  render() {
-    return (
-      <Spring
-        defaultValue={this.getDefaults()}
-        endValue={this.getEnds()}>
-        {current =>
-            <div>
-              {Object.keys(current).map(key => {
-                let {rotate,scale} = current[key].val;
-                let style = {
-                  transform: `
-                  rotate(${rotate}deg)
-                  scale(${scale})`
-                }
-                return (
-                  <div
-                    key={key}
-                    className="block"
-                    style={style}>
-                    {key}
-                  </div>
-                )
-              })}
-            </div>
-        }
-      </Spring>
-    )
-  }
-}
-
-
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
